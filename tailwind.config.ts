38de2ae8dcabd6e2bd37714aa70c6537a6b1e8c7
Weston Odom <weston.odom@gmail.com>
2024-08-20 11:21:18 -0500

    add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,44 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+  daisyui: {
+    themes: [
+      "light",
+      "aqua",
+      "dark",
+      // "cupcake",
+      // "bumblebee",
+      // "emerald",
+      // "corporate",
+      // "synthwave",
+      // "retro",
+      // "cyberpunk",
+      // "valentine",
+      // "halloween",
+      // "garden",
+      // "forest",
+      // "lofi",
+      // "pastel",
+      // "fantasy",
+      // "wireframe",
+      // "black",
+      // "luxury",
+      // "dracula",
+      // "cmyk",
+      // "autumn",
+      // "business",
+      // "acid",
+      // "lemonade",
+      // "night",
+      // "coffee",
+      // "winter",
+      // "dim",
+      // "nord",
+      // "sunset",
+    ],
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
